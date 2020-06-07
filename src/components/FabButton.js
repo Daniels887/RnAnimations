8d5cebca67ae131e145893f220df3bd3de11ec05
Daniel <daniel_sousa.p@outlook.com>
2020-06-07 20:30:21 -0300

    fadeButton with animations

--- src/components/FabButton.js
+++ src/components/FabButton.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
@@ -10,10 +10,72 @@ import {
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function FabButton(props) {
+  let open = 0;
+  const animation = new Animated.Value(0);
+
+  function toogleMenu() {
+    const toValue = open ? 0 : 1;
+
+    Animated.spring(animation, {
+      toValue,
+      friction: 5,
+      useNativeDriver: true,
+    }).start();
+
+    open = !open;
+  }
+
+  const cameraStyle = {
+    transform: [
+      {scale: animation},
+      {
+        translateY: animation.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, -120],
+        }),
+      },
+    ],
+  };
+
+  const heartStyle = {
+    transform: [
+      {scale: animation},
+      {
+        translateY: animation.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, -60],
+        }),
+      },
+    ],
+  };
+
+  const rotation = {
+    transform: [
+      {
+        rotate: animation.interpolate({
+          inputRange: [0, 1],
+          outputRange: ['0deg', '45deg'],
+        }),
+      },
+    ],
+  };
+
   return (
     <View style={[styles.container, props.style]}>
       <TouchableWithoutFeedback>
-        <Animated.View style={[styles.button, styles.menu]}>
+        <Animated.View style={[styles.button, styles.submenu, heartStyle]}>
+          <Icon name="heart" size={15} color="#FFF" />
+        </Animated.View>
+      </TouchableWithoutFeedback>
+
+      <TouchableWithoutFeedback>
+        <Animated.View style={[styles.button, styles.submenu, cameraStyle]}>
+          <Icon name="camera" size={15} color="#FFF" />
+        </Animated.View>
+      </TouchableWithoutFeedback>
+
+      <TouchableWithoutFeedback onPress={toogleMenu}>
+        <Animated.View style={[styles.button, styles.menu, rotation]}>
           <Icon name="plus" size={20} color="#FFF" />
         </Animated.View>
       </TouchableWithoutFeedback>
@@ -46,8 +108,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#00213B',
   },
 
-  labelPlus: {
-    fontSize: 32,
-    color: '#fff',
+  submenu: {
+    width: 48,
+    height: 48,
+    borderRadius: 48 / 2,
+    backgroundColor: '#00213B',
   },
 });
