ecc7132ae4f30a9a09c9d8585724a3b374934374
Daniel <daniel_sousa.p@outlook.com>
2020-06-07 19:24:20 -0300

    add react native vector icons

--- src/components/FabButton.js
+++ src/components/FabButton.js
@@ -1,13 +1,53 @@
 import React from 'react';
-import {View, Text, StyleSheet, TouchableWithoutFeedback} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Animated,
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function FabButton(props) {
-  return <View style={[styles.container, props.style]} />;
+  return (
+    <View style={[styles.container, props.style]}>
+      <TouchableWithoutFeedback>
+        <Animated.View style={[styles.button, styles.menu]}>
+          <Icon name="plus" size={20} color="#FFF" />
+        </Animated.View>
+      </TouchableWithoutFeedback>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     position: 'absolute',
+  },
+
+  button: {
+    position: 'absolute',
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowRadius: 10,
+    shadowColor: '#00213B',
+    shadowOpacity: 0.3,
+    shadowOffset: {
+      height: 10,
+    },
+  },
+
+  menu: {
+    backgroundColor: '#00213B',
+  },
+
+  labelPlus: {
+    fontSize: 32,
+    color: '#fff',
   },
 });
