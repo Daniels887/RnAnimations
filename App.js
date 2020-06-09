55f1004434081dbd19104663fd54b58280fc05c7
Daniel <daniel_sousa.p@outlook.com>
2020-06-08 23:09:06 -0300

    add animation loop

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, Text, View, Animated} from 'react-native';
 import FabButton from './src/components/FabButton';
 
 export default function App() {
+  const animation = new Animated.Value(0);
+
+  const rotation = {
+    transform: [
+      {
+        rotate: animation.interpolate({
+          inputRange: [0, 1],
+          outputRange: ['0deg', '360deg'],
+        }),
+      },
+    ],
+  };
+
+  Animated.loop(
+    Animated.timing(animation, {
+      toValue: 1,
+      duration: 2500,
+      useNativeDriver: true,
+    }),
+    {
+      iterations: 5,
+    },
+  ).start();
+
   return (
     <View style={styles.container}>
-      <FabButton style={{bottom: 80, right: 60}} />
+      <Animated.View
+        style={[
+          {justifyContent: 'center', alignItems: 'center', marginTop: 50},
+          rotation,
+        ]}>
+        <Text>Hello</Text>
+      </Animated.View>
+      <FabButton style={{bottom: 100, right: 60}} />
     </View>
   );
 }
